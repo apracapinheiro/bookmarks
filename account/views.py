2488d181464636a47d85cc8229a0cb00ccbce5a4
andre <apracapinheiro@yahoo.com.br>
2017-06-06 17:32:56 -0300

    Initial commit

---
+++ account/views.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm, UserRegistrationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Cria um novo objeto usuario mas nao grava ainda
+            new_user = user_form.save(commit=False)
+            # Seta a senha escolhida
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Salva o objeto user
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse('Autenticação concluída')
+            else:
+                return HttpResponse('Conta desativada')
+        else:
+            return HttpResponse('Login inválido')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+
+@login_required()
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
